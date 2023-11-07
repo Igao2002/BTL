5771b83b50666acc57616121608c6f83e1206dd4
Igao2002 <olailop139@gmail.com>
2023-11-07 22:39:47 +0700

    next

--- google-maps-directions-tutorial-master/src/App.js
+++ google-maps-directions-tutorial-master/src/App.js
@@ -2,10 +2,7 @@ import { Box, Button, ButtonGroup, Flex, HStack, IconButton, Input, SkeletonText
 import { FaLocationArrow, FaTimes } from 'react-icons/fa'
 import { useJsApiLoader, GoogleMap, Marker, Autocomplete, DirectionsRenderer } from '@react-google-maps/api'
 import { useRef, useState } from 'react'
-// Import the functions you need from the SDKs you need
 import { ref, child, onValue } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-database.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 import { database } from "./Component/firebase"
 import { PolyLineComponent } from "./Component/PolyLine"
 
@@ -18,11 +15,12 @@ function App() {
   })
 
   const [showPolyline, setShowPolyline] = useState(false);
+  const [showInfo, setShowInfo] = useState(false);
+  const [showBox, setShowBox] = useState(false);
   const [map, setMap] = useState(/** @type google.maps.Map */(null))
   const [directionsResponse, setDirectionsResponse] = useState(null)
   const [distance, setDistance] = useState('')
   const [duration, setDuration] = useState('')
-
   /** @type React.MutableRefObject<HTMLInputElement> */
   const originRef = useRef()
   /** @type React.MutableRefObject<HTMLInputElement> */
@@ -31,6 +29,16 @@ function App() {
   if (!isLoaded) {
     return <SkeletonText />
   }
+  
+  let  center = { lat: 20.981159951655183, lng: 105.78742447634367 };
+  // lấy data
+  const dbRef = ref(database);
+  // Lấy dữ liệu vị trí
+  onValue(child(dbRef, `realtime/LGE_LM-V350_7417b07941dd5c2a`), (snapshot) => {
+    const data = snapshot.val();
+    center = { lat: data.latitude, lng: data.longitude }
+  });
+    // console.log(center)
 
   // Tính khoảng cách thời gian
   async function calculateRoute() {
@@ -48,30 +56,19 @@ function App() {
     setDirectionsResponse(results)
     setDistance(results.routes[0].legs[0].distance.text)
     setDuration(results.routes[0].legs[0].duration.text)
+    const route = results.routes[0]; // Lấy tuyến đường đầu tiên
+    const overviewPath = route.overview_path;
+    const coordinates = [];
+    // Trích xuất tọa độ từ tất cả các điểm trên đường đi
+    overviewPath.forEach(path => {
+      coordinates.push({
+        lat: path.lat(),
+        lng: path.lng(),
+      });
+    });
+    // Danh sách tọa độ sẽ nằm trong biến coordinates
+    console.log(coordinates);
   }
-
-  // const pathCoordinates = []
-  let  center = { lat: 20.981159951655183, lng: 105.78742447634367 };
-  //lấy data
-  const dbRef = ref(database);
-
-  // Vị trí
-  // Lấy dữ liệu
-  onValue(child(dbRef, `realtime/LGE_LM-V350_7417b07941dd5c2a`), (snapshot) => {
-    const data = snapshot.val();
-    center = { lat: data.latitude, lng: data.longitude }
-  });
-  console.log(center)
-  // get(child(dbRef, `realtime/LGE_LM-V350_7417b07941dd5c2a`)).then((snapshot) => {
-  //   if (snapshot.exists()) {
-  //     const data = snapshot.val()
-  //     center = { lat: data.latitude, lng: data.longitude }
-  //   } else {
-  //     console.log("No data available");
-  //   }
-  // }).catch((error) => {
-  //   console.error(error);
-  // });
 
   //clear data input
   function clearRoute() {
@@ -82,20 +79,15 @@ function App() {
     destiantionRef.current.value = ''
   }
 
-  const box1 = document.querySelector('.directionBox')
-  function displayDirectionsFunction() {
-    box1.style.display = (box1.style.display === 'none') ? 'block' : 'none';
-  }
-
-  // PolyLine function displayDirections
+  // function show...
   function polyLineFunction() {
     setShowPolyline(!showPolyline);
   }
-
-  //display info
-  const box3 = document.querySelector('.info');
-  function displayInfo() {
-    box3.style.display = (box3.style.display === 'none') ? 'flex' : 'none';
+  function infoFunction() {
+    setShowInfo(!showInfo);
+  }
+  function boxFunction() {
+    setShowBox(!showBox);
   }
 
   return (
@@ -107,30 +99,32 @@ function App() {
       w='100vw'
     >
       <Box className='menu' position='absolute' left={0} top={0} h='100%' w='10%' bgColor='white' zIndex={100} display='flex' alignItems='center' flexDirection='column' gap={50}>
-        <Button colorScheme='pink' type='submit' paddingBottom='30px' paddingTop='30px' width={'80%'} fontSize={20} marginTop={50} onClick={displayDirectionsFunction}>
+        <Button colorScheme='pink' type='submit' paddingBottom='30px' paddingTop='30px' width={'80%'} fontSize={20} marginTop={50} onClick={boxFunction}>
           Tìm đường
         </Button>
         <Button colorScheme='pink' type='submit' paddingBottom='30px' paddingTop='30px' width={'80%'} fontSize={20} onClick={polyLineFunction}>
           Hiển thị đường đi
         </Button>
-        <Button colorScheme='pink' type='submit' paddingBottom='30px' paddingTop='30px' width={'80%'} fontSize={20} onClick={displayInfo}>
+        <Button colorScheme='pink' type='submit' paddingBottom='30px' paddingTop='30px' width={'80%'} fontSize={20} onClick={infoFunction}>
           Thông tin
         </Button>
-        {/*chỗ này sau đẩy dữ liệu vận tốc các thứ lên thì push vào đây.*/}
-        <Box className='info' display='none' flexDirection={'column'} mt={200}>
-          <Box fontSize={20} marginBottom={6} ml={4} mr={2}>
-            Thông tin 1: mot hai ba bon nam sau bay tam
+        {/* box hiển thị thông tin tốc độ vị trí... */}
+        {showInfo && (
+          <Box flexDirection={'column'} mt={200}>
+            <Box fontSize={20} marginBottom={6} ml={4} mr={2}>
+              Thông tin 1: mot hai ba bon nam sau bay tam
+            </Box>
+            <Box fontSize={20} marginBottom={6} ml={4} mr={2}>
+              Tốc độ:
+            </Box>
+            <Box fontSize={20} marginBottom={6} ml={4} mr={2}>
+              Vị trí:
+            </Box>
+            <Box fontSize={20} marginBottom={6} ml={4} mr={2}>
+              Thông tin:
+            </Box>
           </Box>
-          <Box fontSize={20} marginBottom={6} ml={4} mr={2}>
-            Tốc độ:
-          </Box>
-          <Box fontSize={20} marginBottom={6} ml={4} mr={2}>
-            Vị trí:
-          </Box>
-          <Box fontSize={20} marginBottom={6} ml={4} mr={2}>
-            Thông tin:
-          </Box>
-        </Box>
+        )}
       </Box>
       <Box position='absolute' left={0} top={0} h='100%' w='100%'>
         {/* Google Map Box */}
@@ -147,78 +141,69 @@ function App() {
           onLoad={map => setMap(map)}
         >
           <Marker position={center} />
+          {/* đường đi giữa 2 điểm. */}
           {directionsResponse && (
             <DirectionsRenderer directions={directionsResponse} />
           )}
-
-          {/* {showPolyline && (
-            <Polyline
-              path={pathCoordinates}
-              options={{
-                strokeColor: '#FF0000',
-                strokeOpacity: 1.0,
-                strokeWeight: 2,
-              }}
-            />
-          )} */}
+          {/* Đường đi theo firebase */}
           {showPolyline && (
             <PolyLineComponent/>
           )}
-
         </GoogleMap>
       </Box>
-      <Box
-        p={4}
-        borderRadius='lg'
-        m={4}
-        bgColor='white'
-        shadow='base'
-        minW='container.md'
-        zIndex='1'
-        className='directionBox'
-        display={'none'}
-      >
-        <HStack spacing={2} justifyContent='space-between'>
-          <Box flexGrow={1}>
-            <Autocomplete>
-              <Input type='text' placeholder='Điểm đầu' ref={originRef} />
-            </Autocomplete>
-          </Box>
-          <Box flexGrow={1}>
-            <Autocomplete>
-              <Input
-                type='text'
-                placeholder='Điểm cuối'
-                ref={destiantionRef}
-              />
-            </Autocomplete>
-          </Box>
+      {/* box nhập thông tin tìm đường */}
+      {showBox && (
+        <Box
+          p={4}
+          borderRadius='lg'
+          m={4}
+          bgColor='white'
+          shadow='base'
+          minW='container.md'
+          zIndex='1'
+        >
+          <HStack spacing={2} justifyContent='space-between'>
+            <Box flexGrow={1}>
+              <Autocomplete>
+                <Input type='text' placeholder='Điểm đầu' ref={originRef} />
+              </Autocomplete>
+            </Box>
+            <Box flexGrow={1}>
+              <Autocomplete>
+                <Input
+                  type='text'
+                  placeholder='Điểm cuối'
+                  ref={destiantionRef}
+                />
+              </Autocomplete>
+            </Box>
 
-          <ButtonGroup>
-            <Button colorScheme='pink' type='submit' onClick={calculateRoute}>
-              Tính
-            </Button>
+            <ButtonGroup>
+              <Button colorScheme='pink' type='submit' onClick={calculateRoute}>
+                Tính
+              </Button>
+              <IconButton
+                aria-label='center back'
+                icon={<FaTimes />}
+                onClick={clearRoute}
+              />
+            </ButtonGroup>
+          </HStack>
+          <HStack spacing={4} mt={4} justifyContent='space-between'>
+            <Text>Khoảng Cách: {distance} </Text>
+            <Text>Khoảng thời gian: {duration} </Text>
             <IconButton
               aria-label='center back'
-              icon={<FaTimes />}
-              onClick={clearRoute}
+              icon={<FaLocationArrow />}
+              isRound
+              onClick={() => {
+                map.panTo(center)
+                map.setZoom(15)
+              }}
             />
-          </ButtonGroup>
-        </HStack>
-        <HStack spacing={4} mt={4} justifyContent='space-between'>
-          <Text>Khoảng Cách: {distance} </Text>
-          <Text>Khoảng thời gian: {duration} </Text>
-          <IconButton
-            aria-label='center back'
-            icon={<FaLocationArrow />}
-            isRound
-            onClick={() => {
-              map.panTo(center)
-              map.setZoom(15)
-            }}
-          />
-        </HStack>
-      </Box>
+          </HStack>
+        </Box>
+      )}
     </Flex>
   )
 }
--- google-maps-directions-tutorial-master/src/Component/firebase.js
+++ google-maps-directions-tutorial-master/src/Component/firebase.js
@@ -1,10 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-database.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: "btl-iot-401114.firebaseapp.com",
@@ -15,7 +11,6 @@ const firebaseConfig = {
   appId: "1:1048557997036:web:e016038cf608ffc5814ec9"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export const database = getDatabase(app)
